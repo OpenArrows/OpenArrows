344f29ab33069075722c082dde19214c01f611a3
GulgDev <programm.jeremiah@yandex.ru>
2025-05-05 18:13:18 +0500

    Bump GL version and create a base header for the in-memory representation of a circuit map

---
+++ src/arrows.h
@@ -0,0 +1,60 @@
+// Game map in-memory representation definitions (arrow types, signals, chunks,
+// etc.)
+
+// This header is intended to be used both from C and GLSL, so we need to
+// provide some layer of compatiblity
+#ifdef GLSL
+// Enums are not a thing in GLSL. Instead, use uint constants
+#define ENUM(NAME) const NAME
+#define END_ENUM
+
+// GLSL struct members can't have initializers, so it's a no-op
+#define DEFVAL(VALUE)
+#else
+#define ENUM(NAME) enum NAME : unsigned int {
+#define END_ENUM }
+
+#define DEFVAL(VALUE) = VALUE
+
+// `unsigned int` is `uint` in GLSL, but it's simpler to typedef on the C side
+typedef unsigned int uint;
+#endif
+
+#define CHUNK_SIZE 64
+
+#ifdef GLSL
+#define ArrowType uint
+#endif
+ENUM(ArrowType) Void = 0 END_ENUM;
+
+#ifdef GLSL
+#define ArrowRotation uint
+#endif
+ENUM(ArrowRotation) Up = 0, Right = 1, Down = 2, Left = 3 END_ENUM;
+
+#ifdef GLSL
+#define ArrowSignal uint
+#endif
+ENUM(ArrowSignal) None = 0 END_ENUM;
+
+struct Arrow {
+  ArrowType type DEFVAL(Void);
+  ArrowRotation rotation DEFVAL(Up);
+  ArrowSignal signal DEFVAL(None);
+};
+#ifndef GLSL
+typedef struct Arrow Arrow;
+#endif
+
+struct Chunk {
+  Arrow arrows[CHUNK_SIZE * CHUNK_SIZE];
+  uint adjacentChunks[8]; // It's better to cache adjacent chunks than make a
+                          // look-up each time. Adjacent chunks are stored
+                          // clockwise, starting from the top:
+                          //    701
+                          //    6#2
+                          //    543
+};
+#ifndef GLSL
+typedef struct Chunk Chunk;
+#endif
